7557226859926fd4ac26129410286484239cef81
komayu <mayuko.yamaguchi0712@gmail.com>
2026-01-28 11:36:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, CloudRun!"
+    return "Hello, CloudRun0128!"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
